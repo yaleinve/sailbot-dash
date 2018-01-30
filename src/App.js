bf3cffebdc48311dd3fd1d79f4fa786debc7900d
Lincoln Berkley <linc.berkley@yale.edu>
2018-01-29 21:00:42 -0500

    Three-column layout

--- src/App.js
+++ src/App.js
@@ -11,7 +11,10 @@ class App extends Component {
         <Map size="500" rosbridgeAddr="192.168.56.101:9090" />
       </div>,
       <div className={styles.dataCol}>
-        <TextBox name="Test Name" value="test data" width="150px" />
+        <TextBox name="Test Name" value="2nd column" width="150px" />
+      </div>,
+      <div className={styles.controlCol}>
+        <TextBox name="Test Name" value="3rd column" width="150px" />
       </div>
     ];
   }
