bae24e374f338df4fc1814d9e6b8e5fca5427efb
Amanda <ajhwhiz@gmail.com>
2018-02-05 20:06:17 -0500

    changed textbox back to state vals

--- src/App.js
+++ src/App.js
@@ -8,13 +8,22 @@ import Control from './Control';
 
 
 class App extends Component {
+
+    // /* fn passed from App to T.C. in order to change text values
+    //     @param cb = callback from T.C. to change val */
+    // _changeTextValue(cb) {
+    //
+    // }
+
     render() {
         return [
             <div className={styles.mapCol} key="mapCol">
                 <Map size="500" rosbridgeAddr="192.168.56.101:9090"/>
             </div>,
             <div className={styles.dataCol} key="dataCol">
-                <TextContainer />
+                <TextContainer
+                  changeTextValue = {this._changeTextValue}
+                />
             </div>,
             <div className={styles.controlCol} key="controlCol">
                 <Readout size="250" rosbridgeAddr="192.168.56.101:9090"/>
--- src/TextContainer.js
+++ src/TextContainer.js
@@ -6,21 +6,33 @@ import styles from './stylesheets/TextContainer.css';
 class TextContainer extends Component {
   constructor () {
     super();
+    this.state = {
+      speed: '',
+      heading: '',
+      main: '',
+      jib: '',
+      rudder: '',
+      windspeed: '',
+      roll: '',
+      xte: '',
+      vmg: ''
+    }
   }
+
 
   render () {
     return (
       <table className={styles.box} style={{width: "100%"}}>
           <tbody>
-          <TextBox name="Speed" value={this.props.speed} width="150px"/>
-          <TextBox name="Heading" value={this.props.heading} width="150px"/>
-          <TextBox name="Main" value={this.props.main} width="150px"/>
-          <TextBox name="Jib" value={this.props.jib} width="150px"/>
-          <TextBox name="Rudder" value={this.props.rudder} width="150px"/>
-          <TextBox name="Wind Speed" value={this.props.windspeed} width="150px"/>
-          <TextBox name="Roll" value={this.props.roll} width="150px"/>
-          <TextBox name="XTE" value={this.props.xte} width="150px"/>
-          <TextBox name="VMG" value={this.props.vmg} width="150px"/>
+          <TextBox name="Speed" value={this.state.speed} width="150px"/>
+          <TextBox name="Heading" value={this.state.heading} width="150px"/>
+          <TextBox name="Main" value={this.state.main} width="150px"/>
+          <TextBox name="Jib" value={this.state.jib} width="150px"/>
+          <TextBox name="Rudder" value={this.state.rudder} width="150px"/>
+          <TextBox name="Wind Speed" value={this.state.windspeed} width="150px"/>
+          <TextBox name="Roll" value={this.state.roll} width="150px"/>
+          <TextBox name="XTE" value={this.state.xte} width="150px"/>
+          <TextBox name="VMG" value={this.state.vmg} width="150px"/>
           </tbody>
       </table>
     )
