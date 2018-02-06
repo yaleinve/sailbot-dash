4de3bebcae50958bae895efcb0859e6e98a1a128
Amanda <ajhwhiz@gmail.com>
2018-02-05 19:50:49 -0500

    textcontainer waiting to merge

--- src/TextContainer.js
+++ src/TextContainer.js
@@ -10,15 +10,15 @@ class TextContainer extends Component {
     return (
       <table className={styles.box} style={{width: "100%"}}>
           <tbody>
-          <TextBox name="Speed" value="unimplemented" width="150px"/>
-          <TextBox name="Heading" value="unimplemented" width="150px"/>
-          <TextBox name="Main" value="unimplemented" width="150px"/>
-          <TextBox name="Jib" value="unimplemented" width="150px"/>
-          <TextBox name="Rudder" value="unimplemented" width="150px"/>
-          <TextBox name="Wind Speed" value="unimplemented" width="150px"/>
-          <TextBox name="Roll" value="unimplemented" width="150px"/>
-          <TextBox name="XTE" value="unimplemented" width="150px"/>
-          <TextBox name="VMG" value="unimplemented" width="150px"/>
+          <TextBox name="Speed" value={this.state.speed} width="150px"/>
+          <TextBox name="Heading" value={this.state.heading} width="150px"/>
+          <TextBox name="Main" value={this.state.main} width="150px"/>
+          <TextBox name="Jib" value={this.state.} width="150px"/>
+          <TextBox name="Rudder" value={this.state.} width="150px"/>
+          <TextBox name="Wind Speed" value={this.state.} width="150px"/>
+          <TextBox name="Roll" value={this.state.} width="150px"/>
+          <TextBox name="XTE" value={this.state.} width="150px"/>
+          <TextBox name="VMG" value={this.state.} width="150px"/>
           </tbody>
       </table>
     )
