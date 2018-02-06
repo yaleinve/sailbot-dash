2bb051578dd106ea56c572e17142abc0efea46a6
jared weinstein <jaredweinstein@jareds-MacBook-Pro.local>
2018-02-05 21:33:15 -0500

    updated map model

--- src/Map.js
+++ src/Map.js
@@ -3,12 +3,27 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 import MapGL, {Marker} from 'react-map-gl';
 
 import MARKER_STYLE from './marker-style';
-import gps_locs from './test_locs.json';
 
 class Map extends Component {
     constructor() {
       super();
+
+      // bind for callback see:
+      // https://medium.com/@rjun07a/binding-callbacks-in-react-components-9133c0b396c6
+      this._onClick = this._onClick.bind(this)
+
       this.state = {
+        poi: [
+            {"name":"Point 1","coordinates":[41.258245, -72.850291]},
+            {"name":"Point 2","coordinates":[41.258245, -72.858291]},
+            {"name":"Point 3","coordinates":[41.252245, -72.858291]},
+            {"name":"Point 4","coordinates":[41.252245, -72.850291]}],
+        boat_location: [41.256245, -72.855291],
+        destination: [41.256245, -72.855291],
+        path_history: [[41.256245, -72.855291],
+                        [41.256245, -72.855291],
+                        [41.256245, -72.855291],
+                        [41.256245, -72.855291]],
         viewport: {
             width: 400,
             height: 400,
@@ -23,14 +38,20 @@ class Map extends Component {
             touchZoom: true,
             touchRotate: true,
             keyboard: true,
-            doubleClickZoom: true,
+            doubleClickZoom: false,
             minZoom: 0,
             maxZoom: 20,
             minPitch: 0,
-            maxPitch: 85
+            maxPitch: 85,
+            onClick: this._onClick
         }
       };
     };
+
+    _onClick(event) {
+        console.log(event.lngLat)
+        this.state.boat_location = event.lngLat
+    }
 
     _resize = () => {
         this.setState({
@@ -42,7 +63,17 @@ class Map extends Component {
         });
     };
 
-    _onViewportChange = viewport => this.setState({viewport});
+    _onViewportChange(viewport) {
+        this.setState({viewport});
+    }
+
+    _renderBoatPath(path) {
+        return (
+            <Layer type="line" layout={lineLayout} paint={linePaint}>
+                <Feature coordinates={mappedRoute} />
+            </Layer>
+        );
+    }
 
     _renderMarker(station, i) {
         const {name, coordinates} = station;
@@ -61,7 +92,8 @@ class Map extends Component {
                 onViewportChange={(viewport) => this.setState({viewport})}
                 mapStyle="mapbox://styles/mapbox/dark-v9">
                 <style>{MARKER_STYLE}</style>
-                {gps_locs.map(this._renderMarker)}
+                {this.state.poi.map(this._renderMarker)}
+                {this._renderBoatPath(this.state._renderBoatPath)}
             </MapGL>
         );
     }
