26635e70bdc501fe06aa9c8baf1c0fe80bd1d45a
Amanda <ajhwhiz@gmail.com>
2018-02-05 19:56:52 -0500

    added state values to textcontainer

--- src/TextContainer.js
+++ src/TextContainer.js
@@ -5,7 +5,18 @@ import styles from './stylesheets/TextContainer.css';
 
 class TextContainer extends Component {
   constructor () {
-    super()
+    super();
+    this.state = {
+      speed: '',
+      heading: '',
+      main: '',
+      jib: '',
+      rudder: '',
+      windspeed: '',
+      roll: '',
+      xte: '',
+      vmg: ''
+    }
   }
 
   render () {
@@ -15,12 +26,12 @@ class TextContainer extends Component {
           <TextBox name="Speed" value={this.state.speed} width="150px"/>
           <TextBox name="Heading" value={this.state.heading} width="150px"/>
           <TextBox name="Main" value={this.state.main} width="150px"/>
-          <TextBox name="Jib" value={this.state.} width="150px"/>
-          <TextBox name="Rudder" value={this.state.} width="150px"/>
-          <TextBox name="Wind Speed" value={this.state.} width="150px"/>
-          <TextBox name="Roll" value={this.state.} width="150px"/>
-          <TextBox name="XTE" value={this.state.} width="150px"/>
-          <TextBox name="VMG" value={this.state.} width="150px"/>
+          <TextBox name="Jib" value={this.state.jib} width="150px"/>
+          <TextBox name="Rudder" value={this.state.rudder} width="150px"/>
+          <TextBox name="Wind Speed" value={this.state.windspeed} width="150px"/>
+          <TextBox name="Roll" value={this.state.roll} width="150px"/>
+          <TextBox name="XTE" value={this.state.xte} width="150px"/>
+          <TextBox name="VMG" value={this.state.vmg} width="150px"/>
           </tbody>
       </table>
     )
