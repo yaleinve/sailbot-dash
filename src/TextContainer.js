1234c1069069f8177d9b83a45aaa88215a59b726
Lincoln Berkley <linc.berkley@yale.edu>
2018-04-23 21:21:15 -0400

    Add velocity direction display

--- src/TextContainer.js
+++ src/TextContainer.js
@@ -13,6 +13,7 @@ class TextContainer extends Component {
             rudder: '',
             // AirmarData
             speed: '',
+            cog: '',
             heading: '',
             windspeed: '',
             roll: '',
@@ -49,6 +50,7 @@ class TextContainer extends Component {
                 <table className={styles.box} style={{width: "100%"}}>
                 <tbody>
                 <TextBox name="Speed" value={this.state.speed} width="150px"/>
+                <TextBox name="Velocity Direction" value={this.state.cog} width="150px"/>
                 <TextBox name="Heading" value={this.state.heading} width="150px"/>
                 <TextBox name="Main" value={this.state.main} width="150px"/>
                 <TextBox name="Jib" value={this.state.jib} width="150px"/>
@@ -72,8 +74,11 @@ class TextContainer extends Component {
     }
 
     airmarListener(msg) {
+        let cog = msg.cog;
+        if (cog < 0) cog += 360;
         this.setState({
             speed: parseFloat(msg.sog).toFixed(2),
+            cog: parseFloat(cog).toFixed(1),
             heading: parseFloat(msg.heading).toFixed(1),
             windspeed: parseFloat(msg.truWndSpd).toFixed(2),
             roll: parseFloat(msg.amrRoll).toFixed(1),
