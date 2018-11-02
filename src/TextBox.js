c6b0daa31a51b63a7cb9b64f851c86de27d05d7b
jared weinstein <>
2018-11-02 14:07:20 -0400

    adding units

--- src/TextBox.js
+++ src/TextBox.js
@@ -2,10 +2,15 @@ import React from 'react';
 import styles from './stylesheets/TextBox.css';
 
 function TextBox(props) {
+    var val = ""
+    if (props.value) {
+        val = props.value
+        if (props.unit) val = val + props.unit
+    }
     return (
         <tr>
             <th className={styles.th}>{props.name + ":"}</th>
-            <td className={styles.cell}>{props.value}</td>
+            <td className={styles.cell}>{val}</td>
         </tr>
     );
 }
--- src/TextContainer.js
+++ src/TextContainer.js
@@ -49,18 +49,18 @@ class TextContainer extends Component {
         return (
                 <table className={styles.box} style={{width: "100%"}}>
                 <tbody>
-                <TextBox name="Speed" value={this.state.speed} width="150px"/>
+                <TextBox name="Speed" value={this.state.speed} unit={" knots"} width="150px"/>
                 <TextBox name="Velocity Direction" value={this.state.cog} width="150px"/>
-                <TextBox name="Heading" value={this.state.heading} width="150px"/>
-                <TextBox name="Main" value={this.state.main} width="150px"/>
-                <TextBox name="Jib" value={this.state.jib} width="150px"/>
-                <TextBox name="Rudder" value={this.state.rudder} width="150px"/>
-                <TextBox name="Wind Speed" value={this.state.windspeed} width="150px"/>
-                <TextBox name="Roll" value={this.state.roll} width="150px"/>
-                <TextBox name="XTE" value={this.state.xte} width="150px"/>
-                <TextBox name="VMG" value={this.state.vmg} width="150px"/>
-                <TextBox name="Latitude" value={this.state.lat} width="150px"/>
-                <TextBox name="Longitude" value={this.state.long} width="150px"/>
+                <TextBox name="Heading " value={this.state.heading} unit={"°"} width="150px"/>
+                <TextBox name="Main" value={this.state.main} unit={"°"} width="150px"/>
+                <TextBox name="Jib" value={this.state.jib} unit={"°"} width="150px"/>
+                <TextBox name="Rudder" value={this.state.rudder} unit={"°"} width="150px"/>
+                <TextBox name="Wind Speed" value={this.state.windspeed} unit={" knots"} width="150px"/>
+                <TextBox name="Roll" value={this.state.roll} unit={"°"} width="150px"/>
+                <TextBox name="XTE" value={this.state.xte} unit={"m"} width="150px"/>
+                <TextBox name="VMG" value={this.state.vmg} unit={"?"} width="150px"/>
+                <TextBox name="Latitude" value={this.state.lat} unit={"°"} width="150px"/>
+                <TextBox name="Longitude" value={this.state.long} unit={"°"} width="150px"/>
                 <TextBox name="Distance Left" value={this.state.targetRange} width="150px"/>
                 <TextBox name="P.O.S" value={this.state.pointOfSail} width="150px"/>
                 <TextBox name="Target Course" value={this.state.targetCourse} width="150px"/>
